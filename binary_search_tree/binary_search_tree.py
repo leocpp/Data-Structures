d464c4e53568e5ff7ee9f0151d613f593d07a359
Leo Lam <leo.cpp@gmail.com>
2018-07-17 10:53:31 -0700

    binary tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,35 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if (self.value == None):
+        self.value = BinarySearchTree(value)
+    elif (value < self.value):
+        if (self.left == None):
+            self.left = BinarySearchTree(value)
+        else:
+            self.left.insert(value)
+    else:
+        if (self.right == None):
+            self.right = BinarySearchTree(value)
+        else:
+            self.right.insert(value)
+         
 
   def contains(self, target):
-    pass
+    if (self.value == target):
+        return True 
+    elif (target < self.value and self.left != None):
+        return self.left.contains(target)
+    elif (self.right != None):
+        return self.right.contains(target)
+    else:
+        return False
+        
 
   def get_max(self):
-    pass
+    max = self.value
+    p = self.right
+    while (p != None):
+        max = p.value
+        p = p.right
+    return max
